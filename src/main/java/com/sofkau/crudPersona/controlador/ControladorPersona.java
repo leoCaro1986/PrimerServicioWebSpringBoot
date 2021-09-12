06ff71f1dd2c13698602b762141b989b8239c211
Leonardo <leocv0205@gmail.com>
2021-09-11 23:23:42 -0500

    Se crean los primeros dos servicios para consumir nuestro back-end, crear y listar personas

--- src/main/java/com/sofkau/crudPersona/controlador/ControladorPersona.java
+++ src/main/java/com/sofkau/crudPersona/controlador/ControladorPersona.java
@@ -3,9 +3,7 @@ package com.sofkau.crudPersona.controlador;
 import com.sofkau.crudPersona.entidades.Persona;
 import com.sofkau.crudPersona.servicios.InterfasServiciosPersona;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api")
@@ -14,9 +12,13 @@ public class ControladorPersona {
     @Autowired
     private InterfasServiciosPersona servicio;
 
-    @GetMapping(value = "listarPersonas")
+    @GetMapping(value = "/listarPersonas")
     public Iterable<Persona>listarPersonas(){
         return servicio.listar();
     }
 
+    @PostMapping(value = "/guardarPersona")
+    public  Persona guardarPersona(@RequestBody Persona persona){
+        return servicio.guardar(persona);
+    }
 }
--- src/main/java/com/sofkau/crudPersona/servicios/ServicioPersona.java
+++ src/main/java/com/sofkau/crudPersona/servicios/ServicioPersona.java
@@ -1,16 +1,21 @@
 package com.sofkau.crudPersona.servicios;
 
 import com.sofkau.crudPersona.entidades.Persona;
+import com.sofkau.crudPersona.repositorio.InterfasRepositorioPersona;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ServicioPersona implements InterfasServiciosPersona{
 
-
+    @Autowired
+    private InterfasRepositorioPersona data;
 
     @Override
     public List<Persona> listar() {
-        return null;
+        return (List<Persona>) data.findAll();
     }
 
     @Override
@@ -20,7 +25,7 @@ public class ServicioPersona implements InterfasServiciosPersona{
 
     @Override
     public Persona guardar(Persona persona) {
-        return null;
+        return data.save(persona);
     }
 
     @Override
